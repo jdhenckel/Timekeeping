47005a21a866b87736b31730e19b86ea497a6e6b
John Henckel <jdhenckel@gmail.com>
2018-03-16 12:14:12 -0500

    logon page

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 
 
 
-$(document).ready(function(){
-    $(".topbar button").click(function(x){
-//        print(x);
-        $('.childwrapper div').hide();
-        $(".topbar button").removeClass("selected");
-        let b = $(x.target);
-        b.addClass("selected");
-        $('.view' + b.attr('id')).show();
-    });
-});
+$(document).ready(initialize);
 
-function print(x) {
-    console.log(JSON.stringify(x));
+//****** Global data ***********
+
+var token;
+
+// call this one time
+function initialize() {
+    $(".topbar button").click(clickButtonBar);
+    
+    // simulate button click
+    $('#1').click();
+}
+
+
+// click a button on the top bar
+function clickButtonBar(x){
+    $('.childwrapper > div').hide();
+    $(".topbar button").removeClass("selected");
+    let b = $(x.target);
+    b.addClass("selected");
+    $('.view' + b.attr('id')).show();
+}
+
+
+function logon() {
+    let userpw = {
+        username: $('#user').val(),
+        password: $('#pw').val(),
+    };
+    print('attempt logon', userpw);
+    $.post('api/logon', userpw, (data, status) => {
+        print('logon result = ', data, status);
+        token = data;
+        $('#token').text(token);
+    });
+}
+
+
+// for debugging stuff
+function print(...x) {
+    console.log(JSON.stringify(x,null,4));
 }
